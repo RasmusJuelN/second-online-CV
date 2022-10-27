287305ec7d669ca248adf5777fff46afaa28a4de
RasmusJuelN <rasmusn686@hotmail.com>
2022-10-27 14:53:22 +0200

    line skills section moved up

--- js/script.js
+++ js/script.js
@@ -54,13 +54,6 @@ function slideDurationTimeout(slideDuration)
 var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
 window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
 
-$(window).scroll(function() {
-  if ($('.background').length) {
-      if ($(window).scrollTop() == 0 && e.deltaY > 0) {
-          $('.background').removeClass('up-scroll');
-      }
-  }
-});
 
 
 // ------------- SLIDE MOTION ------------- //
